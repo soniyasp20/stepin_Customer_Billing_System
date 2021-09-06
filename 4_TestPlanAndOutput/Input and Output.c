a0a710dcfd95a8f4cd0a919dd595a334b005471a
soniyasp20 <81178250+soniyasp20@users.noreply.github.com>
2021-09-06 19:18:56 +0530

    Add files via upload

---
+++ 4_TestPlanAndOutput/Input and Output.c
@@ -0,0 +1,79 @@
+
+## Library Management ##
+
+1. Add book information
+2. Display New Book Available
+3. Display Highest Price Book
+4. count of available Books
+5. Exit
+
+ Enter one of the above : 1
+
+Enter book name = Ulysses
+Enter publisher name = Joyce
+Enter price = 300
+
+1. Add book information
+2. Display New Book Available
+3. Display Highest Price Book
+4. count of available Books
+5. Exit
+
+ Enter one of the above : 1
+
+Enter book name = Hamlet
+Enter publisher name = Shakespeare
+Enter price = 400
+
+1. Add book information
+2. Display New Book Available
+3. Display Highest Price Book
+4. count of available Books
+5. Exit
+
+ Enter one of the above : 1
+
+Enter book name = Beloved
+Enter publisher name = Morrison
+Enter price = 500
+
+1. Add book information
+2. Display New Book Available
+3. Display Highest Price Book
+4. count of available Books
+5. Exit
+
+ Enter one of the above : 2
+
+ Display New Book Available
+
+ book name = Beloved     author name = Morrison   price = 500.000000
+
+1. Add book information
+2. Display New Book Available
+3. Display Highest Price Book
+4. count of available Books
+5. Exit
+
+ Enter one of the above : 3
+
+ Highest Price Book :500.000000
+
+
+1. Add book information
+2. Display New Book Available
+3. Display Highest Price Book
+4. count of available Books
+5. Exit
+
+ Enter one of the above : 4
+
+ No of books in library : 3
+
+1. Add book information
+2. Display New Book Available
+3. Display Highest Price Book
+4. count of available Books
+5. Exit
+
+ Enter one of the above : 5
